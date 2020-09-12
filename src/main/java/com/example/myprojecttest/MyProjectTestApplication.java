2876fdf616b696560ac845be1f7ca3d66580a238
luyuxiong <gz_yxlu@163.com>
2020-09-12 14:53:03 +0800

    '新建项目'

---
+++ src/main/java/com/example/myprojecttest/MyProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myprojecttest/MyProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
